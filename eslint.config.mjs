0d84e5b65f626381f6f10a189932feed7284cc64
Paul Myburgh <lostpebble@protonmail.com>
2025-04-28 17:11:04 +0200

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import eslint from "@eslint/js";
+import reactCompilerPlugin from "eslint-plugin-react-compiler";
+// @ts-check
+import { defineConfig, globalIgnores } from "eslint/config";
+import tsEslint from "typescript-eslint";
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  tsEslint.configs.recommended,
+  defineConfig([
+    globalIgnores(["**/*/dist/*", ".husky/"]),
+    {
+      plugins: {
+        "react-compiler": reactCompilerPlugin,
+      },
+      rules: {
+        "react-compiler/react-compiler": "error",
+        "react/display-name": "off",
+        // "@typescript-eslint/no-unused-vars": "off",
+        // "@typescript-eslint/no-empty-object-type": "off",
+        // "@typescript-eslint/array-type": "off",
+      },
+    },
+  ]),
+);
